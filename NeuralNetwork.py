91386787f6d05534a628f6384d8647613ff1faa0
craftinix <lulo.flach@gmail.com>
2026-05-07 08:53:10 +0200

    NeuralNetwork

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -1,7 +1,23 @@
 class NeuralNetwork:
 
-    def __init__(self):
-        pass
+    def __init__(self, inputNodes, hiddenNodes, outputNodes, learningRate):
+    # set number of nodes in each input, hidden output layer
+        self.inodes = inputNodes
+        self.hNodes = hiddenNodes
+        self.oNodes = outputNodes
+
+    # learning rate
+        self.lr = learningRate
+    pass
+
+# number of input, hidden & output nodes
+
+    input_nodes = 3
+    hidden_nodes = 3
+    output_nodes = 3
+
+# learning rate is 0.3
+    learning_rate = 0.3
 
     def train(self):
         print("I am training")
@@ -10,6 +26,7 @@ class NeuralNetwork:
     def query(self):
         print("I ask...")
         pass
+#ENDCLASS
 
 # create instance Neural network
 neuralNetwork = NeuralNetwork()
--- main.py
+++ main.py
@@ -5,24 +5,7 @@ import matplotlib.pyplot
 import NeuralNetwork
 
 # initialise the neural network
-def __init__(self, inputNodes, hiddenNodes, outputNodes, learningRate):
-    # set number of nodes in each input, hidden output layer
-    self.inodes = inputNodes
-    self.hNodes = hiddenNodes
-    self.oNodes = outputNodes
 
-    # learning rate
-    self.lr = learningRate
-    pass
-
-# number of input, hidden & output nodes
-
-input_nodes = 3
-hidden_nodes = 3
-output_nodes = 3
-
-# learning rate is 0.3
-learning_rate = 0.3
 
 # create instance of neural network
 neuralNetwork = NeuralNetwork(input_nodes, hidden_nodes, output_nodes, learning_rate)
