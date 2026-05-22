8649974255630dd623d9a71dd071b1661282ef0f
Daniel Geitz <danigeitz@gmail.com>
2026-05-22 10:03:36 +0200

    Forwärts kalkulation + Sigmoid

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -21,19 +21,53 @@ class NeuralNetwork:
         self.wih = numpy.random.normal(0.0, pow(self.iNodes, -0.5), (self.hNodes, self.iNodes))
         self.who = numpy.random.normal(0.0, pow(self.hNodes, -0.5), (self.oNodes, self.hNodes))
 
+    # activation function is the sigmoid function
+        self.activation_function = lambda x: scipy.special.expit(x)
 
-
-    def train(self):
-        print("I am training")
         pass
 
-    def query(self):
-        print("I ask...")
+    # train the neural network
+    def train(self, inputs_list, targets_list):
+        print("I startet training ...")
+          # convert inputs list to 2d array
+        inputs = numpy.array(inputs_list, ndmin=2).T
+        targets = numpy.array(targets_list, ndmin=2).T
+        
+        # calculate signals into hidden layer
+        hidden_inputs = numpy.dot(self.wih, inputs)
+        # calculate the signals emerging from hidden layer
+        hidden_outputs = self.activation_function(hidden_inputs)
+        
+        # calculate signals into final output layer
+        final_inputs = numpy.dot(self.who, hidden_outputs)
+        # calculate the signals emerging from final output layer
+        final_outputs = self.activation_function(final_inputs)
+        
+        #Some debuginformations
+        print("Final output:")
+        print(final_outputs)
         pass
+
+    # query the neural network
+    def query(self, inputs_list):
+         # convert inputs list to 2d array
+        inputs = numpy.array(inputs_list, ndmin=2).T
+        
+        # calculate signals into hidden layer
+        hidden_inputs = numpy.dot(self.wih, inputs)
+        # calculate the signals emerging from hidden layer
+        hidden_outputs = self.activation_function(hidden_inputs)
+        
+        # calculate signals into final output layer
+        final_inputs = numpy.dot(self.who, hidden_outputs)
+        # calculate the signals emerging from final output layer
+        final_outputs = self.activation_function(final_inputs)
+
+        return final_outputs
 #ENDCLASS
 
 
-# number of input, hidden & output nodes
+# number of input, hidden and output nodes
 input_nodes = 3
 hidden_nodes = 3
 output_nodes = 3
@@ -41,9 +75,16 @@ output_nodes = 3
 # learning rate is 0.3
 learning_rate = 0.3
 
-# create instance Neural network
-neuralNetwork = NeuralNetwork(input_nodes,hidden_nodes,output_nodes, learning_rate)
+# create instance of neural network
+n = NeuralNetwork(input_nodes,hidden_nodes,output_nodes, learning_rate)
 
-neuralNetwork.train()
+# train the neural network
+# NEW: Some small test_data
+input_list=[(1.0, 0.5, 0.3), (0.4, 0.9, 0.1)]
+target_list=[(1.0, 0.0, 0.0), (0.0, 1.0, 0.0)]
+n.train(input_list, target_list)
 
-outputs = neuralNetwork.query()
+# test query (doesn't mean anything useful yet)
+output=n.query([1.0, 0.5, -1.5])
+print("Queryoutput:")
+print(output)
